4f0847a6108b9d770fec8b51f228d7e8979b2b16
zanthegreat18 <sulistiyofauzan27@gmail.com>
2024-06-27 00:37:05 +0700

    final edit chapter 1

---
+++ assets/js/zan.js
@@ -0,0 +1,8 @@
+function showPopup(popupId) {
+    document.getElementById(popupId).style.display = "block";
+  }
+  
+  function hidePopup(popupId) {
+    document.getElementById(popupId).style.display = "none";
+  }
+  
